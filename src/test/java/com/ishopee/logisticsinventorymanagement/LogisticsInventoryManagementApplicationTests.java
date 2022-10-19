4fd29e61c1b92e0b079a0013654a82ff647a2b2d
Jitesh Singh <jiteshs101@gmail.com>
2022-10-19 21:49:59 +0530

    Project Environment setup

---
+++ src/test/java/com/ishopee/logisticsinventorymanagement/LogisticsInventoryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ishopee.logisticsinventorymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsInventoryManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

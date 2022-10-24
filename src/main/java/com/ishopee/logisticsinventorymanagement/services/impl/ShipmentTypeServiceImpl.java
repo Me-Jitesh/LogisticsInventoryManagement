35398ddfdfe4e3c1a6331df54d5dd016daf66255
Jitesh Singh <jiteshs101@gmail.com>
2022-10-24 12:04:29 +0530

    Module1-ShipmentType : Business Logic layer

--- src/main/java/com/ishopee/logisticsinventorymanagement/services/impl/ShipmentTypeServiceImpl.java
+++ src/main/java/com/ishopee/logisticsinventorymanagement/services/impl/ShipmentTypeServiceImpl.java
@@ -1,6 +1,13 @@
 package com.ishopee.logisticsinventorymanagement.services.impl;
 
+import com.ishopee.logisticsinventorymanagement.repositories.ShipmentTypeRepo;
 import com.ishopee.logisticsinventorymanagement.services.IShipmentTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ShipmentTypeServiceImpl implements IShipmentTypeService {
+
+    @Autowired
+    private ShipmentTypeRepo shipmentTypeRepo;
 }

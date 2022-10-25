8065cc19eeda7d06bed868dc41e430504eed9262
Jitesh Singh <jiteshs101@gmail.com>
2022-10-25 12:20:57 +0530

    Module1-ShipmentType : UI design

--- src/main/java/com/ishopee/logisticsinventorymanagement/controllers/ShipmentTypeController.java
+++ src/main/java/com/ishopee/logisticsinventorymanagement/controllers/ShipmentTypeController.java
@@ -3,6 +3,7 @@ package com.ishopee.logisticsinventorymanagement.controllers;
 import com.ishopee.logisticsinventorymanagement.services.IShipmentTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,4 +13,8 @@ public class ShipmentTypeController {
     @Autowired
     private IShipmentTypeService service;
 
+    @GetMapping("/register")
+    public String showRegister() {
+        return "ShipmentTypeRegister";
+    }
 }

fe8aebd3023882ad8c299546af68713ad24964c6
Jitesh Singh <jiteshs101@gmail.com>
2022-10-25 13:14:41 +0530

    Module1-ShipmentType : PL - save data

--- src/main/java/com/ishopee/logisticsinventorymanagement/controllers/ShipmentTypeController.java
+++ src/main/java/com/ishopee/logisticsinventorymanagement/controllers/ShipmentTypeController.java
@@ -1,9 +1,13 @@
 package com.ishopee.logisticsinventorymanagement.controllers;
 
+import com.ishopee.logisticsinventorymanagement.models.ShipmentType;
 import com.ishopee.logisticsinventorymanagement.services.IShipmentTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,6 +19,14 @@ public class ShipmentTypeController {
 
     @GetMapping("/register")
     public String showRegister() {
+        return "ShipmentTypeRegister";
+    }
+
+    @PostMapping("/save")
+    public String saveShipmentType(@ModelAttribute ShipmentType shipmentType, Model model) {
+        Integer id = service.saveShipmentType(shipmentType);
+        String msg = "ShipmentType " + id + " registered successfully";
+        model.addAttribute("message", msg);
         return "ShipmentTypeRegister";
     }
 }

40dc7c7af8f0db439268a6d979b3227744600c14
Jitesh Singh <jiteshs101@gmail.com>
2022-11-02 10:49:20 +0530

    Module1-ShipmentType : DL - shipment code ajax validation

--- src/main/java/com/ishopee/logisticsinventorymanagement/repositories/ShipmentTypeRepo.java
+++ src/main/java/com/ishopee/logisticsinventorymanagement/repositories/ShipmentTypeRepo.java
@@ -2,6 +2,10 @@ package com.ishopee.logisticsinventorymanagement.repositories;
 
 import com.ishopee.logisticsinventorymanagement.models.ShipmentType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ShipmentTypeRepo extends JpaRepository<ShipmentType, Integer> {
+
+    @Query("SELECT count(ST.shipCode) from ShipmentType ST where ST.shipCode=:shipmentCode")
+    Integer getShipmentCode(String shipmentCode);
 }

65dc8039c95973125d0ec94ac18defac1983c808
Jitesh Singh <jiteshs101@gmail.com>
2022-11-08 09:23:44 +0530

    Module2-MeasurementUnitSystem : Data Access Layer Completed

---
+++ src/main/java/com/ishopee/logisticsinventorymanagement/repositories/MusRepo.java
@@ -0,0 +1,7 @@
+package com.ishopee.logisticsinventorymanagement.repositories;
+
+import com.ishopee.logisticsinventorymanagement.models.Mus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MusRepo extends JpaRepository<Mus, Integer> {
+}

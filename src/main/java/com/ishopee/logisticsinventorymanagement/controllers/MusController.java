75b0349ca9a1ef6bb70df37dbe8d2c81e832c79d
Jitesh Singh <jiteshs101@gmail.com>
2022-11-09 21:19:19 +0530

    Module2-MeasurementUnitSystem : PL - Mus data delete

--- src/main/java/com/ishopee/logisticsinventorymanagement/controllers/MusController.java
+++ src/main/java/com/ishopee/logisticsinventorymanagement/controllers/MusController.java
@@ -5,10 +5,7 @@ import com.ishopee.logisticsinventorymanagement.services.IMusService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -36,6 +33,16 @@ public class MusController {
     public String getAllMus(Model model) {
         List<Mus> list = service.getAllMus();
         model.addAttribute("list", list);
+        return "MusData";
+    }
+
+    @GetMapping("/delete")
+    public String deletelShipmentType(@RequestParam Integer id, Model model) {
+        service.deleteMus(id);
+        String msg = " MUS  " + id + " Deleted !!";
+        List<Mus> list = service.getAllMus();
+        model.addAttribute("list", list);
+        model.addAttribute("message", msg);
         return "MusData";
     }
 }

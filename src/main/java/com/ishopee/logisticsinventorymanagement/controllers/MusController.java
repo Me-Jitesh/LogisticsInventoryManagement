7472ab3ec2e53f4f4debd9f88b6ec2eeff073654
Jitesh Singh <jiteshs101@gmail.com>
2022-11-09 18:52:44 +0530

    Module2-MeasurementUnitSystem : PL - Mus Register

--- src/main/java/com/ishopee/logisticsinventorymanagement/controllers/MusController.java
+++ src/main/java/com/ishopee/logisticsinventorymanagement/controllers/MusController.java
@@ -1,9 +1,13 @@
 package com.ishopee.logisticsinventorymanagement.controllers;
 
+import com.ishopee.logisticsinventorymanagement.models.Mus;
 import com.ishopee.logisticsinventorymanagement.services.IMusService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,6 +19,14 @@ public class MusController {
 
     @GetMapping("/register")
     public String showMusRegister() {
+        return "MusRegister";
+    }
+
+    @PostMapping("/save")
+    public String saveShipmentType(@ModelAttribute Mus mus, Model model) {
+        Integer id = service.saveMus(mus);
+        String msg = "MUS " + id + " registered successfully";
+        model.addAttribute("message", msg);
         return "MusRegister";
     }
 }

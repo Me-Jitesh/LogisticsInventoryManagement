d7afe6cf016ab82a292892a5d299adfc287dab41
Jitesh Singh <jiteshs101@gmail.com>
2022-11-09 18:49:45 +0530

    Module2-MeasurementUnitSystem : SL - Mus Register

--- src/main/java/com/ishopee/logisticsinventorymanagement/services/IMusService.java
+++ src/main/java/com/ishopee/logisticsinventorymanagement/services/IMusService.java
@@ -1,4 +1,9 @@
 package com.ishopee.logisticsinventorymanagement.services;
 
+import com.ishopee.logisticsinventorymanagement.models.Mus;
+
 public interface IMusService {
+
+    Integer saveMus(Mus mus);
+
 }
--- src/main/java/com/ishopee/logisticsinventorymanagement/services/impl/MusServiceImpl.java
+++ src/main/java/com/ishopee/logisticsinventorymanagement/services/impl/MusServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ishopee.logisticsinventorymanagement.services.impl;
 
+import com.ishopee.logisticsinventorymanagement.models.Mus;
 import com.ishopee.logisticsinventorymanagement.repositories.MusRepo;
 import com.ishopee.logisticsinventorymanagement.services.IMusService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,4 +11,10 @@ public class MusServiceImpl implements IMusService {
 
     @Autowired
     private MusRepo musRepo;
+
+    @Override
+    public Integer saveMus(Mus mus) {
+        mus = musRepo.save(mus);
+        return mus.getId();
+    }
 }

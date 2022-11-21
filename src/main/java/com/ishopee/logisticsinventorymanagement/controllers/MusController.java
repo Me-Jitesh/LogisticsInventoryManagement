21dccddb70ef274de5fc3795df5810387a2ac443
Jitesh Singh <jiteshs101@gmail.com>
2022-11-21 18:37:08 +0530

    Module2-MeasurementUnitSystem : Logging

--- src/main/java/com/ishopee/logisticsinventorymanagement/controllers/MusController.java
+++ src/main/java/com/ishopee/logisticsinventorymanagement/controllers/MusController.java
@@ -2,6 +2,8 @@ package com.ishopee.logisticsinventorymanagement.controllers;
 
 import com.ishopee.logisticsinventorymanagement.models.Mus;
 import com.ishopee.logisticsinventorymanagement.services.IMusService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,6 +17,7 @@ public class MusController {
 
     @Autowired
     private IMusService service;
+    private static final Logger LOG = LoggerFactory.getLogger(MusController.class);
 
     @GetMapping("/register")
     public String showMusRegister() {
@@ -23,56 +26,88 @@ public class MusController {
 
     @PostMapping("/save")
     public String saveMus(@ModelAttribute Mus mus, Model model) {
-        Integer id = service.saveMus(mus);
-        String msg = "MUS " + id + " registered successfully";
-        model.addAttribute("message", msg);
+        try {
+            Integer id = service.saveMus(mus);
+            LOG.debug("RECORD IS CREATED WITH ID {}", id);
+            String msg = "MUS " + id + " registered successfully";
+            model.addAttribute("message", msg);
+        } catch (Exception e) {
+            LOG.error("unable to process save request due to {}", e.getMessage());
+            e.printStackTrace();
+        }
+        LOG.info("ABOUT TO GO UI PAGE MusRegister ! ");
         return "MusRegister";
     }
 
     @GetMapping("/all")
     public String getAllMus(Model model) {
-        List<Mus> list = service.getAllMus();
-        model.addAttribute("list", list);
+        LOG.info("ENTERED INTO getAllMus METHOD");
+        try {
+            List<Mus> list = service.getAllMus();
+            LOG.debug("FETCHED ALL RECORDS");
+            model.addAttribute("list", list);
+        } catch (Exception e) {
+            LOG.error("unable to process getAllMus request due to {}", e.getMessage());
+            e.printStackTrace();
+        }
+        LOG.info("ABOUT TO GO UI PAGE MusData ! ");
         return "MusData";
     }
 
     @GetMapping("/delete")
     public String deleteMus(@RequestParam Integer id, Model model) {
+        LOG.info("ENTERED INTO DELETE METHOD");
         try {
             service.deleteMus(id);
             String msg = " MUS  " + id + " Deleted !!";
+            LOG.debug(msg);
             List<Mus> list = service.getAllMus();
             model.addAttribute("list", list);
             model.addAttribute("message", msg);
         } catch (Exception e) {
+            LOG.error("unable to process delete request : {}", e.getMessage());
             e.printStackTrace();
             List<Mus> list = service.getAllMus();
             model.addAttribute("list", list);
             model.addAttribute("message", e.getMessage());
         }
+        LOG.info("ABOUT TO GO UI PAGE MusData ! ");
         return "MusData";
     }
 
     @GetMapping("/edit")
     public String editMus(@RequestParam Integer id, Model model) {
         String page;
+        LOG.info("ENTERED INTO EDIT METHOD");
         try {
             Mus musItem = service.getMus(id);
+            LOG.debug("RECORD FOUND WITH ID {}", id);
             model.addAttribute("musItem", musItem);
             page = "MusEdit";
         } catch (Exception e) {
+            LOG.error("unable to process edit request due to {}", e.getMessage());
             e.printStackTrace();
             List<Mus> list = service.getAllMus();
             model.addAttribute("list", list);
             model.addAttribute("message", e.getMessage());
             page = "MusData";
         }
+        LOG.info("ABOUT TO GO UI PAGE {} !", page);
         return page;
     }
 
     @PostMapping("/update")
     public String updateMus(@ModelAttribute Mus mus) {
-        service.updateMus(mus);
+        LOG.info("ENTERED INTO UPDATE METHOD");
+        try {
+            service.updateMus(mus);
+            LOG.debug("RECORD IS UPDATED FOR ID {}", mus.getId());
+
+        } catch (Exception e) {
+            LOG.error("unable to process update request due to {}", e.getMessage());
+            e.printStackTrace();
+        }
+        LOG.info("REDIRECTING TO FETCH ALL RECORD ! ");
         return "redirect:all";
     }
 }

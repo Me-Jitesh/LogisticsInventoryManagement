bf41269698ead5ad88ccd1df45ec6c1fdd16dbb7
Jitesh Singh <jiteshs101@gmail.com>
2022-11-30 21:47:42 +0530

    Module3-OrderMethod : DL - Bug Fixed

--- src/main/java/com/ishopee/logisticsinventorymanagement/repositories/OrderMethodRepo.java
+++ src/main/java/com/ishopee/logisticsinventorymanagement/repositories/OrderMethodRepo.java
@@ -4,7 +4,7 @@ import com.ishopee.logisticsinventorymanagement.models.OrderMethod;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-public interface OrderMethodRepo extends JpaRepository<Integer, OrderMethod> {
+public interface OrderMethodRepo extends JpaRepository<OrderMethod, Integer> {
 
     @Query("SELECT count(orderCode) FROM OrderMethod  WHERE orderCode=:orderCode")
     Integer isOrderCodeExist(String orderCode);

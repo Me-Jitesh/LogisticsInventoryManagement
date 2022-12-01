7c7f96d496420b740b2a99934719be60f3242152
Jitesh Singh <Jiteshs101@gmail.com>
2022-12-01 20:54:16 +0530

    Module3-OrderMethod : Order Method Register Completed

---
+++ src/main/java/com/ishopee/logisticsinventorymanagement/controllers/OrderMethodController.java
@@ -0,0 +1,44 @@
+package com.ishopee.logisticsinventorymanagement.controllers;
+
+import com.ishopee.logisticsinventorymanagement.models.OrderMethod;
+import com.ishopee.logisticsinventorymanagement.services.IOrderMethodService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/om")
+public class OrderMethodController {
+    private static final Logger LOG = LoggerFactory.getLogger(MusController.class);
+
+    @Autowired
+    private IOrderMethodService service;
+
+    @GetMapping("/register")
+    public String orderMethodRegister() {
+        return "OrderMethodRegister";
+    }
+
+    @PostMapping("/save")
+    public String saveOrderMethod(@ModelAttribute OrderMethod orderMethod, Model model) {
+        LOG.info("ENTERED INTO saveOrderMethod");
+        try {
+            System.out.println(orderMethod);
+            Integer id = service.saveOrderMethod(orderMethod);
+            LOG.debug("RECORD IS CREATED WITH ID {}", id);
+            String msg = "order method " + id + " registered successfully";
+            model.addAttribute("message", msg);
+        } catch (Exception e) {
+            LOG.error("UNABLE TO PROCESS SAVE REQUEST DUE TO {}", e.getMessage());
+            e.printStackTrace();
+        }
+        LOG.info("ABOUT TO GO UI PAGE OrderMethodRegister ! ");
+        return "OrderMethodRegister";
+    }
+}

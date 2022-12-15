506fa71cd2f958467ad6b267d89d04da61b0ccf1
Jitesh Singh <Jiteshs101@gmail.com>
2022-12-15 12:13:03 +0530

    Module4-Analytics : Fetch All Visitors

--- src/main/java/com/ishopee/logisticsinventorymanagement/controllers/AnalyticsController.java
+++ src/main/java/com/ishopee/logisticsinventorymanagement/controllers/AnalyticsController.java
@@ -7,9 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 public class AnalyticsController {
@@ -36,5 +38,11 @@ public class AnalyticsController {
         modelAndView.setView(new AnalyticsPdfView());
         modelAndView.addObject("visitors", visitorService.getAllVisitors());
         return modelAndView;
+    }
+
+    @ResponseBody
+    @GetMapping("/all")
+    public List<Visitor> fetchAll() {
+        return visitorService.getAllVisitors();
     }
 }

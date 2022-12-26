5f04c36721220ad56bc33f98f1b5d6a5ee783669
Jitesh Singh <Jiteshs101@gmail.com>
2022-12-26 12:39:06 +0530

    Module4-Analytics : Display Chart

--- src/main/java/com/ishopee/logisticsinventorymanagement/repositories/VisitorRepo.java
+++ src/main/java/com/ishopee/logisticsinventorymanagement/repositories/VisitorRepo.java
@@ -2,10 +2,16 @@ package com.ishopee.logisticsinventorymanagement.repositories;
 
 import com.ishopee.logisticsinventorymanagement.models.Visitor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface VisitorRepo extends JpaRepository<Visitor, Integer> {
     // Named Query To Get Recent 10 Visitors Details By Timestamp
     List<Visitor> findTop10ByOrderByLocaleTimestampDesc();
+
+    // Chart
+    @Query("SELECT visitor.locale.countryCode,count(visitor.locale.countryCode) from Visitor visitor group by visitor.locale.countryCode")
+    List<Object[]> getVistorCountryCodeAndCount();
+
 }
--- src/main/java/com/ishopee/logisticsinventorymanagement/utilities/VisitorUtility.java
+++ src/main/java/com/ishopee/logisticsinventorymanagement/utilities/VisitorUtility.java
@@ -7,11 +7,21 @@ import com.ishopee.logisticsinventorymanagement.models.VisitorLocation;
 import com.maxmind.geoip2.DatabaseReader;
 import com.maxmind.geoip2.exception.GeoIp2Exception;
 import com.maxmind.geoip2.model.CityResponse;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartUtils;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.StandardChartTheme;
+import org.jfree.chart.renderer.category.BarRenderer;
+import org.jfree.chart.renderer.category.StandardBarPainter;
+import org.jfree.chart.ui.RectangleInsets;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.data.general.DefaultPieDataset;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
+import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.net.InetAddress;
@@ -22,6 +32,7 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.sql.Timestamp;
 import java.time.Instant;
+import java.util.List;
 
 @Component
 public class VisitorUtility {
@@ -32,8 +43,8 @@ public class VisitorUtility {
         Visitor visitor = new Visitor();
         String ip = extractIP(httpServletRequest);
         visitor.setIpAddress(ip);
-        VisitorLocation locale = extractLocaleByGeoLite2(ip, httpServletRequest.getServletContext().getRealPath("/extra_resources/GeoLite2-City-DB/"));
-//        VisitorLocation locale = extractLocaleByIP2Location(ip);
+//        VisitorLocation locale = extractLocaleByGeoLite2(ip, httpServletRequest.getServletContext().getRealPath("/extra_resources/GeoLite2-City-DB/"));
+        VisitorLocation locale = extractLocaleByIP2Location(ip);
         visitor.setLocale(locale);
         return visitor;
     }
@@ -131,5 +142,74 @@ public class VisitorUtility {
             e.printStackTrace();
         }
         return visitorLocation;
+    }
+
+    public void generatePieChart(String path, List<Object[]> data) {
+        try {
+            // Prepare Data Source
+            DefaultPieDataset dataset = new DefaultPieDataset();
+            for (Object[] ob : data)
+                dataset.setValue(ob[0].toString(), Double.valueOf(ob[1].toString()));
+            // Create JFreeChart Object
+            JFreeChart chart = ChartFactory.createPieChart3D("PIE", dataset);
+            // Beautification
+            chart.getPlot().setBackgroundPaint(Color.WHITE);
+//            chart.getTitle().setVisible(false);
+            chart.getTitle().setFont(new Font("Ubuntu", Font.PLAIN, 15));
+            // Save As Image
+            ChartUtils.saveChartAsPNG(new File(path + "/pie.png"), chart, 200, 200);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void generateBarChart(String path, List<Object[]> data) {
+        try {
+            // Prepare Data Source
+            DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+            for (Object[] ob : data)
+                dataset.setValue(Double.valueOf(ob[1].toString()), ob[0].toString(), "");
+            // Create JFreeChart Object
+            JFreeChart chart = ChartFactory.createBarChart("BAR ", "COUNTRY", "COUNT", dataset);
+            // Beautification
+            beautifyChart(chart);
+            // Save As Image
+            ChartUtils.saveChartAsPNG(new File(path + "/bar.png"), chart, 200, 200);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void beautifyChart(JFreeChart chart) {
+        String fontName = "Lucida Sans";
+        StandardChartTheme theme = (StandardChartTheme) StandardChartTheme.createJFreeTheme();
+        theme.setTitlePaint(Color.decode("#4572a7"));
+        //title
+        theme.setExtraLargeFont(new Font(fontName, Font.BOLD, 14));
+        //axis-title
+        theme.setLargeFont(new Font(fontName, Font.BOLD, 15));
+        theme.setRegularFont(new Font(fontName, Font.PLAIN, 11));
+        theme.setRangeGridlinePaint(Color.decode("#C0C0C0"));
+        theme.setPlotBackgroundPaint(Color.white);
+        theme.setChartBackgroundPaint(Color.white);
+        theme.setGridBandPaint(Color.red);
+        theme.setAxisOffset(new RectangleInsets(0, 0, 0, 0));
+        theme.setBarPainter(new StandardBarPainter());
+        theme.setAxisLabelPaint(Color.decode("#666666"));
+        theme.apply(chart);
+        chart.getCategoryPlot().setOutlineVisible(false);
+        chart.getCategoryPlot().getRangeAxis().setAxisLineVisible(false);
+        chart.getCategoryPlot().getRangeAxis().setTickMarksVisible(false);
+        chart.getCategoryPlot().setRangeGridlineStroke(new BasicStroke());
+        chart.getCategoryPlot().getRangeAxis().setTickLabelPaint(Color.decode("#666666"));
+        chart.getCategoryPlot().getDomainAxis().setTickLabelPaint(Color.decode("#666666"));
+        chart.setTextAntiAlias(true);
+        chart.setAntiAlias(true);
+        BarRenderer rend = (BarRenderer) chart.getCategoryPlot().getRenderer();
+        rend.setShadowVisible(true);
+        rend.setShadowXOffset(2);
+        rend.setShadowYOffset(0);
+        rend.setShadowPaint(Color.decode("#C0C0C0"));
+        rend.setMaximumBarWidth(0.1);
     }
 }

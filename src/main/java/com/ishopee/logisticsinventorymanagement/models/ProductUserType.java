24a35489cab3afc4ebe7f71068887e16f319f3c0
Jitesh Singh <jiteshs101@gmail.com>
2022-12-30 12:49:15 +0530

    Module5-ProductUserType : Data Layer Finished

--- src/main/java/com/ishopee/logisticsinventorymanagement/models/ProductUserType.java
+++ src/main/java/com/ishopee/logisticsinventorymanagement/models/ProductUserType.java
@@ -2,6 +2,31 @@ package com.ishopee.logisticsinventorymanagement.models;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "product_user_type_tab")
 @Data
 public class ProductUserType {
+    @Id
+    @GeneratedValue(generator = "prodId")
+    @SequenceGenerator(name = "prodId", sequenceName = "prod_usr_seq")
+    @Column(name = "prod_user_id_col")
+    private Integer id;
+    @Column(name = "prod_user_type_col")
+    private String userType;
+    @Column(name = "prod_user_code_col")
+    private String userCode;
+    @Column(name = "prod_user_for_col")
+    private String userFor;
+    @Column(name = "prod_user_email_col")
+    private String userEmail;
+    @Column(name = "prod_user_contact_col")
+    private String userContact;
+    @Column(name = "prod_user_idtupe_col")
+    private String userIdType;
+    @Column(name = "prod_user_ifother_col")
+    private String ifOther;
+    @Column(name = "prod_user_idnum_col")
+    private String userIdNumber;
 }
--- src/main/java/com/ishopee/logisticsinventorymanagement/repositories/ProductUserTypeRepo.java
+++ src/main/java/com/ishopee/logisticsinventorymanagement/repositories/ProductUserTypeRepo.java
@@ -2,6 +2,18 @@ package com.ishopee.logisticsinventorymanagement.repositories;
 
 import com.ishopee.logisticsinventorymanagement.models.ProductUserType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProductUserTypeRepo extends JpaRepository<ProductUserType, Integer> {
+
+    @Query("SELECT count(userCode) FROM ProductUserType  WHERE userCode=:userCode")
+    Integer isUserCodeExist(String userCode);
+
+    @Query("SELECT count(userCode) FROM ProductUserType  WHERE userCode=:userCode AND id !=:id")
+    Integer isUserCodeExistForEdit(String userCode, Integer id);
+
+    @Query("SELECT PU.userType,count(PU.userType) from ProductUserType PU group by PU.userType")
+    List<Object[]> getProductUserTypeAndCount();
 }

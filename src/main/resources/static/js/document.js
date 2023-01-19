ab28f037d0ee45736fc57f0b1cd5685498eb344f
Jitesh Singh <Jiteshs101@gmail.com>
2023-01-19 10:07:52 +0530

    Master : Document JQuery Size Validation

--- src/main/resources/static/js/document.js
+++ src/main/resources/static/js/document.js
@@ -19,10 +19,21 @@ $(document).ready(function () {
         let fileName = file.name;
         let fExtension = fileName.substring(fileName.lastIndexOf(".") + 1);
         let allowedExt = ["png", "jpg", "jpeg", "txt", "doc", "docx", "pdf"];
+        let fileSize = file.size;
 
         if ($.inArray(fExtension, allowedExt) == -1) {
             $("#docDataError").show();
             $("#docDataError").html(" * allowed only " + allowedExt);
+            $("#docDataError").css("color", "red");
+            docDataError = false;
+        } else if (fileSize < 1024 * 10) {//10kb
+            $("#docDataError").show();
+            $("#docDataError").html(" * allowed min size 10kb");
+            $("#docDataError").css("color", "red");
+            docDataError = false;
+        } else if (fileSize > 1024 * 1000) {//1 mb
+            $("#docDataError").show();
+            $("#docDataError").html(" * allowed max size 1mb");
             $("#docDataError").css("color", "red");
             docDataError = false;
         } else {

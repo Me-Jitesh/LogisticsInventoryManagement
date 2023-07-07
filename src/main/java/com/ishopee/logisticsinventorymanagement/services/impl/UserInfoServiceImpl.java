9214d4a51130720f907d8ccd9a78b56c8f4709ba
Jitesh Singh <Jiteshs101@gmail.com>
2023-07-07 20:36:03 +0530

    Module12-Security : Encrypting the Password

--- src/main/java/com/ishopee/logisticsinventorymanagement/services/impl/UserInfoServiceImpl.java
+++ src/main/java/com/ishopee/logisticsinventorymanagement/services/impl/UserInfoServiceImpl.java
@@ -4,6 +4,7 @@ import com.ishopee.logisticsinventorymanagement.models.UserInfo;
 import com.ishopee.logisticsinventorymanagement.repositories.UserInfoRepo;
 import com.ishopee.logisticsinventorymanagement.services.IUserInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,9 +14,14 @@ public class UserInfoServiceImpl implements IUserInfoService {
 
     @Autowired
     private UserInfoRepo repo;
+    @Autowired
+    private BCryptPasswordEncoder encoder;
 
     @Override
     public Integer saveUserInfo(UserInfo userInfo) {
+        String pwd = userInfo.getPassword();
+        String encryptedPw = encoder.encode(pwd);
+        userInfo.setPassword(encryptedPw);
         return repo.save(userInfo).getId();
     }
 

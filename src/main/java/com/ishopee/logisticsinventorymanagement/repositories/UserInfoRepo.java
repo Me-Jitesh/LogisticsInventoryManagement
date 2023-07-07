46c6bbe57cbd353b655d247cd5edabb07c59666a
Jitesh Singh <Jiteshs101@gmail.com>
2023-07-07 21:35:49 +0530

    Module12-Security : Configured UserDetailsService and get User

--- src/main/java/com/ishopee/logisticsinventorymanagement/repositories/UserInfoRepo.java
+++ src/main/java/com/ishopee/logisticsinventorymanagement/repositories/UserInfoRepo.java
@@ -3,7 +3,12 @@ package com.ishopee.logisticsinventorymanagement.repositories;
 import com.ishopee.logisticsinventorymanagement.models.UserInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserInfoRepo extends JpaRepository<UserInfo, Integer> {
 
     Boolean existsByEmail(String email);
+
+    Optional<UserInfo> findByEmail(String email);
+
 }
--- src/main/java/com/ishopee/logisticsinventorymanagement/services/impl/UserInfoServiceImpl.java
+++ src/main/java/com/ishopee/logisticsinventorymanagement/services/impl/UserInfoServiceImpl.java
@@ -1,16 +1,23 @@
 package com.ishopee.logisticsinventorymanagement.services.impl;
 
+import com.ishopee.logisticsinventorymanagement.exceptions.UserInfoNotFoundException;
 import com.ishopee.logisticsinventorymanagement.models.UserInfo;
 import com.ishopee.logisticsinventorymanagement.repositories.UserInfoRepo;
 import com.ishopee.logisticsinventorymanagement.services.IUserInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
-public class UserInfoServiceImpl implements IUserInfoService {
+public class UserInfoServiceImpl implements IUserInfoService, UserDetailsService {
 
     @Autowired
     private UserInfoRepo repo;
@@ -29,5 +36,39 @@ public class UserInfoServiceImpl implements IUserInfoService {
     public List<UserInfo> getAllUserInfo() {
         return repo.findAll();
     }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserInfo userInfo = repo.findByEmail(username).orElseThrow(() -> new UserInfoNotFoundException("USER DOES NOT EXIST !"));
+        return new User(userInfo.getEmail(), userInfo.getPassword(), userInfo.getRoles().stream().map(role -> new SimpleGrantedAuthority(role.getRole().name())).collect(Collectors.toSet()));
+    }
+
+    /*    OLD METHOD     */
+
+/*
+//     Needed 3 things i.e. username , password, authorities, and pass it to WebSecurityConfigurerAdapter again(internally)
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+//         Load current user by username i.e. email
+        Optional<UserInfo> opt = repo.findByEmail(username);
+
+        UserInfo userInfo;
+        Set<GrantedAuthority> authorities;
+
+        if (opt.isPresent()) {
+//             Convert UserInfo into User of Security
+            userInfo = opt.get();
+//            Convert Userinfo Roles into User Roles format and give it  to GrantedAuthority
+            authorities = new HashSet<>();
+            for (Role role : userInfo.getRoles()) {
+                authorities.add(new SimpleGrantedAuthority(role.getRole().name()));
+            }
+        } else {
+            throw new UserInfoNotFoundException("USER DOES NOT EXIST !");
+        }
+        return new User(userInfo.getEmail(), userInfo.getPassword(), authorities);
+    }
+*/
 
 }

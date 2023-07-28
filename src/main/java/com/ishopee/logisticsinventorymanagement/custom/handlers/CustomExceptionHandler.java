7e9b837d290e8330047af6f65021c1b59920a041
Jitesh Singh <Jiteshs101@gmail.com>
2023-07-28 16:01:47 +0530

    Module10-RestAPI : Global Exception Handling For ShipmentType&OrderMethod

--- src/main/java/com/ishopee/logisticsinventorymanagement/custom/handlers/CustomExceptionHandler.java
+++ src/main/java/com/ishopee/logisticsinventorymanagement/custom/handlers/CustomExceptionHandler.java
@@ -2,6 +2,8 @@ package com.ishopee.logisticsinventorymanagement.custom.handlers;
 
 import com.ishopee.logisticsinventorymanagement.custom.error.ErrorType;
 import com.ishopee.logisticsinventorymanagement.exceptions.MusNotFoundException;
+import com.ishopee.logisticsinventorymanagement.exceptions.OrderMethodNotFoundException;
+import com.ishopee.logisticsinventorymanagement.exceptions.ShipmentTypeNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,5 +17,15 @@ public class CustomExceptionHandler {
     @ExceptionHandler(MusNotFoundException.class)
     public ResponseEntity<ErrorType> handleMusNotFound(MusNotFoundException mnfe) {
         return new ResponseEntity<>(new ErrorType(new Date().toString(), "MUS", mnfe.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(ShipmentTypeNotFoundException.class)
+    public ResponseEntity<ErrorType> handleShipementNotFound(ShipmentTypeNotFoundException stnfe) {
+        return new ResponseEntity<>(new ErrorType(new Date().toString(), "Shipment Type", stnfe.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(OrderMethodNotFoundException.class)
+    public ResponseEntity<ErrorType> handleOrderMethodNotFound(OrderMethodNotFoundException omnfe) {
+        return new ResponseEntity<>(new ErrorType(new Date().toString(), "Order Method", omnfe.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
